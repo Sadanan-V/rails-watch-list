93619603a8416c4f664b1c7324a881f6b327d505
sadanan vorlaruxtara <sadanan.vorlaruxtara@gmail.com>
2024-08-08 17:02:37 +0900

    Completed the rails-watch-list first part

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :lists do
-    member do
-      resources :bookmarks, only: [:new, :create, :destroy]
-    end
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
   end
+
+  resources :bookmarks, only: [:destroy]
 end
